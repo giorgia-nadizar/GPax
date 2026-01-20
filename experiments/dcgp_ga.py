52167890f31b28b95a2cbcc336515a5fef07f702
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2026-01-20 16:29:07 +0100

    exps

--- experiments/dcgp_ga.py
+++ experiments/dcgp_ga.py
@@ -192,7 +192,6 @@ def run_sym_reg_ga(config: Dict):
 
 
 if __name__ == '__main__':
-    n_gens = 100
     conf = {
         "solver": {
             "n_nodes": 15,
@@ -223,31 +222,33 @@ if __name__ == '__main__':
         elif key == "constants_optimization":
             conf["constants_optimization"] = value
 
-    for seed in range(10):
-        conf["seed"] = seed
-        for w_f, w_in, b_f, b_in in [(True, False, False, False), (False, True, False, False),
-                                     (True, False, True, False), (False, True, True, False),
-                                     (True, False, False, True), (False, True, False, True),
-                                     (False, False, False, False)]:
-            if not (w_f or w_in) and conf["constants_optimization"] not in ["mutation", "automl0", "gaussian"]:
-                continue
-            conf["solver"]["weighted_inputs"] = w_in
-            conf["solver"]["weighted_functions"] = w_f
-            conf["solver"]["weighted_program_inputs"] = False
-            conf["solver"]["biased_inputs"] = b_in
-            conf["solver"]["biased_functions"] = b_f
-            conf["n_gens"] = n_gens
-            extra = conf["constants_optimization"]
-            extra += f"_win" if w_in else ""
-            extra += f"_wfn" if w_f else ""
-            extra += f"_bin" if b_in else ""
-            extra += f"_bfn" if b_f else ""
-            # extra += f"_wpgs" if w_pgs else ""
-            extra += "_n" if conf["solver"].get("weights_initialization") == "natural" else ""
-            conf["run_name"] = "ga2_" + conf["problem"].replace("/", "_") + "_" + extra + "_" + str(conf["seed"])
-            print(conf["run_name"])
-            if os.path.exists(f"../results/{conf['run_name']}.pickle"):
-                print("run already done!")
-            else:
-                print("running")
-                run_sym_reg_ga(conf)
+    for opt, n_gens in [("adam", 100), ("gaussian", 1000)]:
+        conf["constants_optimization"] = opt
+        conf["n_gens"] = n_gens
+        for seed in range(10):
+            conf["seed"] = seed
+            for w_f, w_in, b_f, b_in in [(True, False, False, False), (False, True, False, False),
+                                         (True, False, True, False), (False, True, True, False),
+                                         (True, False, False, True), (False, True, False, True),
+                                         (False, False, False, False)]:
+                if not (w_f or w_in) and conf["constants_optimization"] not in ["mutation", "automl0", "gaussian"]:
+                    continue
+                conf["solver"]["weighted_inputs"] = w_in
+                conf["solver"]["weighted_functions"] = w_f
+                conf["solver"]["weighted_program_inputs"] = False
+                conf["solver"]["biased_inputs"] = b_in
+                conf["solver"]["biased_functions"] = b_f
+                extra = conf["constants_optimization"]
+                extra += f"_win" if w_in else ""
+                extra += f"_wfn" if w_f else ""
+                extra += f"_bin" if b_in else ""
+                extra += f"_bfn" if b_f else ""
+                # extra += f"_wpgs" if w_pgs else ""
+                extra += "_n" if conf["solver"].get("weights_initialization") == "natural" else ""
+                conf["run_name"] = "ga2_" + conf["problem"].replace("/", "_") + "_" + extra + "_" + str(conf["seed"])
+                print(conf["run_name"])
+                if os.path.exists(f"../results/{conf['run_name']}.pickle"):
+                    print("run already done!")
+                else:
+                    print("running")
+                    run_sym_reg_ga(conf)
